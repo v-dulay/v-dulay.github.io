d343aaef987910acf3ac635a3f8dcecccb4ff7da
v-dulay <vincedulay6@gmail.com>
2025-08-07 18:10:30 +0800

    Refactor import statements for consistency and clarity

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/v-dulay.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
